5d2b6980b6aec769c6e3f9894dd4ec683f75a9d1
engintruder <engkwangseob@gmail.com>
2016-06-27 20:08:39 +0900

    OpenGDS Mobile Clinet Library Structure Change (Closure)     - Complate MapBis.js     - Add MapManager.js

---
+++ src/js/MapsManager.js
@@ -0,0 +1,16 @@
+goog.provide('openGDSMobile.MapManager');
+goog.require('openGDSMobile.util.applyOptions');
+
+
+
+/**
+ * @constructor
+ * @param {String} _mapDIV - 지도 DIV 객체
+ * @param {Object} _options - 지도 관련 옵션 (JSON 객체)
+ * {
+ *
+ * }
+ */
+openGDSMobile.MapManager = function (_layerDIV, _visObj, _options) {
+
+};
