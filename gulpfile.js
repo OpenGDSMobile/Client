8a28279604c73d2e13f19da0104fc69e23551511
engintruder <engintruder@gmail.com>
2016-10-11 02:35:04 +0900

    Add gulp local server environment

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
+var web_sever = require('gulp-webserver');
 var jsdoc = require('gulp-jsdoc3');
-var closureCompiler = require('gulp-closure-compiler');
+//var concat = require('gulp-concat');
+//var closureCompiler = require('gulp-closure-compiler');
 
 gulp.task('combine-js', [], function() {
 	return gulp.src('src/old/*.js')
@@ -10,32 +11,54 @@ gulp.task('combine-js', [], function() {
 });
 
 gulp.task('doc', function (cb){
-
     gulp.src(['README.md', './src/js/*.js'], {read: false})
     .pipe(jsdoc(cb));
 });
 
-/*
-gulp.task('closure-compiler-debug', function() {
-	return gulp.src([
-		'src/js/!*.js',
-		'closure-library/closure/goog/!**.js'])
-		.pipe(closureCompiler({
-			compilerPath: 'bower_components/closure-compiler/compiler.jar',
-			fileName: 'dist/openGDSMobile-debug.js',
-			compilerFlags: {
-				entry_point: [
-					'openGDSMobile','openGDSMobile.MapVis'
-				],
-				/!*closure_entry_point: 'openGDSMobile',*!/
-				/!*only_closure_dependencies: true,
-				warning_level: 'VERBOSE'*!/
-			}
+gulp.task('webserver', function() {
+	gulp.src('./')
+		.pipe(web_sever({
+			fallback: 'index.html',
+			livereload: true,
+			/*directoryListing: true,*/
+			open: true,
+			port : 8888
 		}))
-		.pipe(gulp.dest('dist'));
-});
-*/
+})
 
 //gulp.task('default', ['combine-js']);
 gulp.task('default', ['doc']);
 
+
+/*
+ gulp.task('closure-compiler-debug', function() {
+ return gulp.src([
+ 'src/js/!*.js',
+ 'closure-library/closure/goog/!**.js'])
+ .pipe(closureCompiler({
+ compilerPath: 'bower_components/closure-compiler/compiler.jar',
+ fileName: 'dist/openGDSMobile-debug.js',
+ compilerFlags: {
+ entry_point: [
+ 'openGDSMobile','openGDSMobile.MapVis'
+ ],
+ /!*closure_entry_point: 'openGDSMobile',*!/
+ /!*only_closure_dependencies: true,
+ warning_level: 'VERBOSE'*!/
+ }
+ }))
+ .pipe(gulp.dest('dist'));
+ });
+ */
+
+/**
+ * Install Method...
+ *
+ * npm install
+ * npm install --save-dev gulp
+ * sudo npm install --global gulp-cli
+ * npm install --save-dev gulp-webserver
+ *
+ *if gulp error..
+ *    $ sudo ln -s /usr/bin/nodejs /usr/bin/node
+ */
